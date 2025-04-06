9eb2f426c4ddf67ffeb8105bc274aa3eebf439d8
bon0512 <nonstop00450@gmail.com>
2025-04-06 23:29:16 +0900

    코딩테스트합격자되기 자바편 연습

--- src/stack/StackPractice.java
+++ src/stack/StackPractice.java
@@ -5,6 +5,7 @@ public class StackPractice {
     public static void main(String[] args) {
 
 
-        System.out.println("하위연!!");
+        System.out.println("안녕하세여");
+        System.out.println("코딩테스트 연습");
     }
 }
