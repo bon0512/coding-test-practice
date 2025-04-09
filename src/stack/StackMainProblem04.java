3090fa6f57e5f53c2f22c9d777879a79686ac262
bon0512 <201800450@hufs.ac.kr>
2025-04-10 00:27:08 +0900

    stack 연습

---
+++ src/stack/StackMainProblem04.java
@@ -0,0 +1,56 @@
+package stack;
+import java.util.*;
+public class StackMainProblem04 {
+
+
+    private static int solution(int[][] board, int[] moves){
+
+        Stack<Integer>[] stackLane = new Stack[board.length];
+
+        for(int i =0;i<stackLane.length;i++){
+            stackLane[i] = new Stack<>();
+        }
+
+        for(int i = board.length-1; i>=0;i--){
+            for(int j = 0; j<board[i].length; j++){
+                if(board[i][j]>0){
+                    stackLane[j].push(board[i][j]);
+                }
+            }
+        }
+
+
+        Stack<Integer> bucket = new Stack<>();
+        int answer = 0;
+
+        for(int i =0; i<moves.length;i++){
+
+            if(stackLane[moves[i]-1].isEmpty()) continue;
+
+            int doll = stackLane[moves[i] - 1].pop();
+
+            if(bucket.isEmpty()){
+                bucket.push(doll);
+            }
+            else{
+                if(bucket.peek()==doll){
+                    bucket.pop();
+                    answer+=2;
+                }
+                else{
+                    bucket.push(doll);
+                }
+
+            }
+
+
+        }
+
+
+
+
+
+        return answer;
+    }
+
+}
