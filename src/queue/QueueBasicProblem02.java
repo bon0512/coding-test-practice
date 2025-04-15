4ef3c93de96a33dca5fc5c61ff56e4afaabe2250
bon0512 <201800450@hufs.ac.kr>
2025-04-15 23:17:01 +0900

    Queue 연습

---
+++ src/queue/QueueBasicProblem02.java
@@ -0,0 +1,51 @@
+package queue;
+
+import java.util.*;
+
+public class QueueBasicProblem02 {
+
+
+
+    private static int[] solution(int[] progresses, int[] speeds){
+
+        List<Integer> answer=  new ArrayList<>();
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+
+
+
+        for(int i =0;i<progresses.length;i++){
+            int remainTime = 100 - progresses[i];
+            if(remainTime%speeds[i]==0){
+               arrayDeque.addLast(remainTime/speeds[i]);
+            }
+            else{
+               arrayDeque.addLast((remainTime/speeds[i]) + 1);
+            }
+        }
+        int count=1;
+        int tmpTime = arrayDeque.pollFirst();
+        while(!arrayDeque.isEmpty()){
+
+            if(arrayDeque.peek()<=tmpTime){
+                count++;
+                tmpTime=arrayDeque.pollFirst();
+            }
+            else{
+                answer.add(count);
+                tmpTime = arrayDeque.pollFirst();
+                count=1;
+
+            }
+        }
+
+        answer.add(count);
+
+
+
+        int[] answer1 = new int[answer.size()];
+        for (int i = 0; i < answer1.length; i++) {
+            answer1[i] = answer.get(i);
+        }
+        return answer1;
+    }
+}
