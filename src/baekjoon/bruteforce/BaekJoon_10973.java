2c4729e268a5508a47c7df04d68faf810c472019
nonstop00450 <201800450@hufs.ac.kr>
2025-09-18 15:21:31 +0900

    10973

---
+++ src/baekjoon/bruteforce/BaekJoon_10973.java
@@ -0,0 +1,60 @@
+package baekjoon.bruteforce;
+
+import java.util.Scanner;
+
+public class BaekJoon_10973 {
+
+
+    static void main() {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int pivot = -1;
+        for (int i = N-1; i >0; i--) {
+            if(arr[i-1]>arr[i]){
+                pivot = i-1;
+                break;
+            }
+
+        }
+
+        if(pivot ==-1){
+            System.out.println(-1);
+            return ;
+        }
+        int maxIndex = pivot+1;
+        for (int i = pivot+1; i < N; i++) {
+            if(arr[pivot]>arr[i] && arr[maxIndex]<arr[i]){
+                maxIndex = i;
+            }
+        }
+
+        int temp = arr[maxIndex];
+        arr[maxIndex] = arr[pivot];
+        arr[pivot] = temp;
+
+        int left = pivot+1;
+        int right = N -1;
+        while(left<right){
+            int t = arr[right];
+            arr[right]=arr[left];
+            arr[left] =t;
+            left++;
+            right--;
+        }
+
+        for (int i = 0; i < N; i++) {
+            System.out.print(arr[i]+" ");
+        };
+
+    }
+}
