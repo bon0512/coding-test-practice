d99505b01eeb18ac4051694349a9c52c0e8554b0
nonstop00450 <201800450@hufs.ac.kr>
2025-09-19 11:43:02 +0900

    n과m

---
+++ src/baekjoon/bruteforce/BaekJoon_15665.java
@@ -0,0 +1,66 @@
+package baekjoon.bruteforce;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * N과 M 12
+ *
+ */
+
+public class BaekJoon_15665 {
+
+
+
+    static int N, M;
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        arr = new int[N];
+        visited = new boolean[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        dfs(0,0,new int[M]);
+        System.out.print(sb);
+
+
+    }
+
+    private static void dfs(int start,int depth, int[] temp) {
+
+        if(depth==M){
+            for(int i : temp){
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return ;
+        }
+
+
+
+        int prev = -1;
+
+        for (int i = start; i < N; i++) {
+            if(prev!=arr[i]){
+                temp[depth] = arr[i];
+                prev = arr[i];
+                dfs(i,depth+1,temp);
+
+            }
+        }
+
+    }
+
+
+}
