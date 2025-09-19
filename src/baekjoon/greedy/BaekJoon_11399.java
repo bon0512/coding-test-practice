9a97fd6dc4294dfcc58a520cf0f22818be71bf4f
nonstop00450 <201800450@hufs.ac.kr>
2025-09-19 14:14:01 +0900

    11399

---
+++ src/baekjoon/greedy/BaekJoon_11399.java
@@ -0,0 +1,28 @@
+package baekjoon.greedy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BaekJoon_11399 {
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr=  new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        int sum =0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                sum+=arr[j];
+            }
+        }
+        System.out.println(sum);
+    }
+}
