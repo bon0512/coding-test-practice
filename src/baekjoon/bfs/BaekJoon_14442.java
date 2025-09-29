5d60060bcae83bb125d58c21dbf12c29651e84f0
nonstop00450 <201800450@hufs.ac.kr>
2025-09-29 15:16:37 +0900

    14442

--- src/baekjoon/bfs/BaekJoon_14442.java
+++ src/baekjoon/bfs/BaekJoon_14442.java
@@ -8,8 +8,8 @@ public class BaekJoon_14442 {
 
     static int N, M, K;
     static int[][] grid;
-    static boolean[][] visited;
-    static int answer =0;
+    static boolean[][][] visited;
+    static int answer = 0;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
 
@@ -19,43 +19,43 @@ public class BaekJoon_14442 {
         M = sc.nextInt();
         K = sc.nextInt();
         grid = new int[N][M];
-        visited = new boolean[N][M];
+        visited = new boolean[N][M][K+1];
 
         sc.nextLine();
         for (int i = 0; i < N; i++) {
             String s = sc.nextLine();
             for (int j = 0; j < M; j++) {
-                grid[i][j] = s.charAt(j)-'0';
+                grid[i][j] = s.charAt(j) - '0';
             }
         }
 
 
-        if(bfs(0,0)){
+        if (bfs()) {
             System.out.println(answer);
-        }else{
+        } else {
             System.out.println(-1);
         }
 
     }
 
-    private static boolean bfs(int x, int y) {
+    private static boolean bfs() {
         Queue<int[]> queue = new LinkedList<>();
-        int k = K;
 
 
-        visited[y][x] = true;
-        queue.add(new int[]{x, y, 1, k});
+
+        visited[0][0][0] = true;
+        queue.add(new int[]{0, 0, 1, 0});
 
         while (!queue.isEmpty()) {
 
             int[] poll = queue.poll();
             int curX = poll[0];
             int curY = poll[1];
-            System.out.println("현재 좌표 X : "+curX +" Y : "+ curY);
+            //System.out.println("현재 좌표 X : " + curX + " Y : " + curY);
             int curCount = poll[2];
             int curK = poll[3];
 
-            if(curX== M-1 && curY == N-1){
+            if (curX == M - 1 && curY == N - 1) {
                 answer = curCount;
                 return true;
             }
@@ -64,14 +64,13 @@ public class BaekJoon_14442 {
                 int nx = curX + dx[i];
                 int ny = curY + dy[i];
 
-                if(nx>=0 &&ny>=0 && nx<M && ny<N && !visited[ny][nx]){
-                    if(grid[ny][nx]==1 && curK>0){
-                        int tempK = curK -1;
-                        visited[ny][nx] =true;
-                        queue.add(new int[]{nx,ny,curCount+1,tempK});
-                    } else if(grid[ny][nx]==0){
-                        visited[ny][nx] =true;
-                        queue.add(new int[]{nx,ny,curCount+1,curK});
+                if (nx >= 0 && ny >= 0 && nx < M && ny < N ) {
+                    if (grid[ny][nx] == 1 && curK<K && !visited[ny][nx][curK+1]) {
+                        visited[ny][nx][curK+1] = true;
+                        queue.add(new int[]{nx, ny, curCount + 1, curK+1});
+                    } else if (grid[ny][nx] == 0 && !visited[ny][nx][curK]) {
+                        visited[ny][nx][curK] = true;
+                        queue.add(new int[]{nx, ny, curCount + 1, curK});
                     }
 
                 }
