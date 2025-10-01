d47664f00ba7207bd86c0c2d9dea9b22614b7a56
nonstop00450 <201800450@hufs.ac.kr>
2025-10-01 15:31:13 +0900

    7569

--- src/baekjoon/bfs/BaekJoon_7569.java
+++ src/baekjoon/bfs/BaekJoon_7569.java
@@ -11,11 +11,11 @@ public class BaekJoon_7569 {
     static int[][][] grid;
     static boolean[][][] visited;
 
-    static int[] dx = {1,-1,0,0,0,0};
-    static int[] dy = {0,0,1,-1,0,0};
-    static int[] dz  ={0,0,0,0,-1,1};
+    static int[] dx = {1, -1, 0, 0, 0, 0};
+    static int[] dy = {0, 0, 1, -1, 0, 0};
+    static int[] dz = {0, 0, 0, 0, -1, 1};
     static int leastDays = -1;
-    static int days=0;
+    static int days = 0;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -31,7 +31,7 @@ public class BaekJoon_7569 {
         boolean isAll = true;
 
 
-        for (int i = 0; i <H; i++) {
+        for (int i = 0; i < H; i++) {
             for (int j = 0; j < N; j++) {
                 for (int k = 0; k < M; k++) {
                     grid[j][k][i] = sc.nextInt();
@@ -41,12 +41,12 @@ public class BaekJoon_7569 {
         }
 
 
-        for (int i = 0; i <H; i++) {
+        for (int i = 0; i < H; i++) {
             for (int j = 0; j < N; j++) {
                 for (int k = 0; k < M; k++) {
-                    if(grid[j][k][i]==1 &&!visited[j][k][i]){
-                        queue.add(new int[]{k,j,i,0});
-                        visited[j][k][i] =true;
+                    if (grid[j][k][i] == 1 && !visited[j][k][i]) {
+                        queue.add(new int[]{k, j, i, 0});
+                        visited[j][k][i] = true;
                     }
 
                 }
@@ -55,10 +55,10 @@ public class BaekJoon_7569 {
 
         leastDays = bfs(queue);
 
-        for (int i = 0; i <H; i++) {
+        for (int i = 0; i < H; i++) {
             for (int j = 0; j < N; j++) {
                 for (int k = 0; k < M; k++) {
-                    if(grid[j][k][i]==0){
+                    if (grid[j][k][i] == 0) {
                         isAll = false;
                         break;
                     }
@@ -67,44 +67,40 @@ public class BaekJoon_7569 {
             }
         }
 
-        if(!isAll){
+        if (!isAll) {
             //System.out.println("이거 실행");
             System.out.println(-1);
-            return ;
+            return;
         }
 
         System.out.println(leastDays);
 
 
-
-
-
-
     }
 
-    static private int bfs(Queue<int[]> queue){
+    static private int bfs(Queue<int[]> queue) {
         int maxDays = 0;
 
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int[] poll = queue.poll();
             int curX = poll[0];
             int curY = poll[1];
             int curZ = poll[2];
             int curDay = poll[3];
 
-            maxDays = Math.max(curDay,maxDays);
+            maxDays = Math.max(curDay, maxDays);
 
             for (int i = 0; i < 6; i++) {
                 int nx = curX + dx[i];
                 int ny = curY + dy[i];
                 int nz = curZ + dz[i];
 
-                if(nx>=0 && ny>=0 && nz>=0 && nx<M && ny<N && nz<H){
+                if (nx >= 0 && ny >= 0 && nz >= 0 && nx < M && ny < N && nz < H) {
 
-                    if(grid[ny][nx][nz]==0 && !visited[ny][nx][nz]){
+                    if (grid[ny][nx][nz] == 0 && !visited[ny][nx][nz]) {
                         visited[ny][nx][nz] = true;
-                        grid[ny][nx][nz]=1;
-                        queue.add(new int[]{nx,ny,nz,curDay+1});
+                        grid[ny][nx][nz] = 1;
+                        queue.add(new int[]{nx, ny, nz, curDay + 1});
                     }
 
                 }
