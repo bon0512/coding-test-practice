66c0b430038d5ca17bd9fc296b61d9717c0aaa0d
bon0512 <nonstop00450@gmail.com>
2025-10-05 03:11:46 +0900

    6603

--- src/baekjoon/implementation/BaekJoon_21610.java
+++ src/baekjoon/implementation/BaekJoon_21610.java
@@ -14,6 +14,7 @@ public class BaekJoon_21610 {
     static int[] dx = {0, -1, -1, 0, 1, 1, 1, 0, -1};
     static int[] dy = {0, 0, -1, -1, -1, 0, 1, 1, 1};
     static int[] numberFour = {2, 4, 6, 8};
+
     public static void main(String[] args) {
 
         System.out.println(-8 % 5);
@@ -33,6 +34,7 @@ public class BaekJoon_21610 {
         }
 
         for (int i = 0; i < 1; i++) {
+
             dir = sc.nextInt();
             street = sc.nextInt();
             int y1 = N - 1, x1 = 0;
@@ -43,108 +45,28 @@ public class BaekJoon_21610 {
             int[] rows = new int[]{x1, x2, x3, x4};
 
 
-//            System.out.println("시작 좌표");
-//            for (int j = 0; j < 4; j++) {
-//                System.out.print("y : "+cols[j]+" ");
-//                System.out.println("x : "+rows[j]);
-//            };
-
             //1번
-            for (int j = 0; j < cols.length; j++) {
+            for (int j = 0; j < 4; j++) {
+                cols[j] = (cols[j]+dy[dir] * street) %N;
+                rows[j] = (rows[j]+dx[dir] * street)%N;
 
-                cols[j] = cols[j] + (dy[dir] * street);
-                rows[j] = rows[j] + (dx[dir] * street);
+                if(cols[j]<0)cols[j] = (cols[j]+N)%N;
+                if(rows[j]<0)rows[j] = (rows[j]+N)%N;
+
             }
-
-//            System.out.println("중간점검");
-//
-//            for (int j = 0; j < 4; j++) {
-//                System.out.print("y : "+cols[j]+" ");
-//                System.out.println("x : "+rows[j]);
-//            }
-
 
             //2번
-            for (int j = 0; j < cols.length; j++) {
 
-                if (cols[j] < 0) {
-                    cols[j] = N - Math.abs(cols[j] % 5);
-                } else if (cols[j] >= N) {
-                    cols[j] = cols[j] % N;
-                }
-
-                if (rows[j] < 0) {
-                    rows[j] = N - Math.abs(rows[j] % 5);
-                } else if (rows[j] >= N) {
-                    rows[j] = rows[j] % N;
-                }
-            }
-
-
-
-//            //System.out.println((i+1)+ "번째 실행 ");
-            for (int j = 0; j < 4; j++) {
-                System.out.print("y : "+cols[j]+" ");
-                System.out.println("x : "+rows[j]);
-            }
-
-
-            //System.out.println("여기까지 완료");
-
-
-//            for (int[] ints : grid) {
-//                System.out.println(Arrays.toString(ints));
-//            };
-
-
-
-            // 구름 배열 설정
-
-            for (int[] ints : grid) {
-                System.out.println(Arrays.toString(ints));
-            };
-
-            System.out.println("좌표");
 
             for (int j = 0; j < 4; j++) {
-                System.out.print("y : "+cols[j]+" ");
-                System.out.println("x : "+rows[j]);
+                isCloud[cols[j]][rows[j]] = true;
             }
 
-            for (int j = 0; j < 4; j++) {
-                isCloud[cols[j]][rows[j]]= true;
-            }
-
-
-            System.out.println("구름 상황");
-            for (boolean[] booleans : isCloud) {
-                System.out.println(Arrays.toString(booleans));
-            };
-
-
-            //2번 비가 내리는거임 이거 구름배열로 해야함
-            for (int j = 0; j < cols.length; j++) {
+            for (int j = 0; j < N; j++) {
                 for (int k = 0; k < N; k++) {
-                    if(isCloud[j][k]) grid[j][k]++;
+                    if (isCloud[j][k]) grid[j][k]++;
                 }
             }
-
-
-
-            System.out.println();
-            for (int[] ints : grid) {
-                System.out.println(Arrays.toString(ints));
-            };
-
-            /*System.out.println("이동한 구름에서 처음으로 비내리기");
-            for (int[] ints : grid) {
-                System.out.println(Arrays.toString(ints));
-            };*/
-
-            System.out.println();
-
-
-
 
 
             for (int j = 0; j < 4; j++) {
@@ -153,49 +75,34 @@ public class BaekJoon_21610 {
                 isCloud[tempY][tempX] = true;
             }
 
-//            System.out.println("구름 상황");
-//            for (boolean[] booleans : isCloud) {
-//                System.out.println(Arrays.toString(booleans));
-//            };
-
-            //System.out.println();
-
-
             for (int j = 0; j < 4; j++) {
 
                 int curY = cols[j];
                 int curX = rows[j];
 
-                //System.out.println("현재 진행 상황 :  좌표 Y : " +curY + " X : "+curX  + " "+grid[curY][curX]);
 
-                for (int k = 2; k <= 8; k+=2) {
+                for (int k = 2; k <= 8; k += 2) {
 
                     int ny = curY + dy[k];
                     int nx = curX + dx[k];
 
                     if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
-                        //System.out.println("통과된 좌표 들 " +ny +" "+ nx +"현재값  "+grid[curY][curX] +" 좌표값 : "+grid[ny][nx]);
-                        if(grid[ny][nx]>0) grid[curY][curX]++;
+                        if (grid[ny][nx] > 0) grid[curY][curX]++;
                     }
 
                 }
 
 
-                /*for (int[] ints : grid) {
-                    System.out.println(Arrays.toString(ints));
-                };*/
-
-                System.out.println();
-
-
             }
             for (int j = 0; j < N; j++) {
                 for (int k = 0; k < N; k++) {
-                    if(grid[j][k]>=2){
-
-                        if(isCloud[j][k]) continue;
-                        else{
-                            grid[j][k]-=2;
+                    if (grid[j][k] >= 2) {
+                        if ((j == cols[0] || j == cols[1] || j == cols[2] || j == cols[3]) && (
+                                k == rows[0] || k == rows[1] || k == rows[2] || k == rows[3])) {
+                            continue;
+                        } else {
+                            isCloud[j][k] = true;
+                            grid[j][k] -= 2;
                         }
                     }
                 }
@@ -207,18 +114,21 @@ public class BaekJoon_21610 {
                 isCloud[tempY][tempX] = false;
             }
 
+            System.out.println("마지막 배열");
+            for (int[] ints : grid) {
+                System.out.println(Arrays.toString(ints));
+            }
+            ;
         }
 
         int answer = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                answer+=grid[i][j];
+                answer += grid[i][j];
             }
         }
 
-        for (int[] ints : grid) {
-            System.out.println(Arrays.toString(ints));
-        };
+
         System.out.println(answer);
 
     }
